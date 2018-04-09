358c9f2ec285791757dd6869bbdaebcd6f9a8d32
davidpatrick <david.patrick@auth0.com>
2018-04-08 20:12:38 -0700

    Adding MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 require('dotenv').config();
+
+const mongoDbUrl = `mongodb://admin:${
+  process.env.MONGO_PASS
+}@ds239359.mlab.com:39359/devfeud`;
+
+let db;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('listening on 3000');
+MongoClient.connect(mongoDbUrl, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db('devfeud');
+
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  });
 });
 
 app.get('/', (req, res) => {
@@ -15,6 +28,10 @@ app.get('/', (req, res) => {
 });
 
 app.post('/question', (req, res) => {
-  console.log('Hellooooooooooooooooo!', req.body);
-  res.sendStatus(200);
+  db.collection('questions').save(req.body, (error, result) => {
+    if (error) return res.send(500, { error });
+
+    console.log('saved to database');
+    res.sendStatus(200);
+  });
 });
