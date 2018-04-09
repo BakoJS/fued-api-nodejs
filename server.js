e249d820bf2e086ef85c0dd0561ab18c55918efd
davidpatrick <david.patrick@auth0.com>
2018-04-08 18:17:41 -0700

    Handling form data
    add body-parser

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log('listening on 3000');
@@ -7,4 +11,9 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/question', (req, res) => {
+  console.log('Hellooooooooooooooooo!', req.body);
+  res.sendStatus(200);
 });
